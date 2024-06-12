858e2883aa591991083303c8b477458c28a92cb7
leviRa3 <levi@wizmacau.com>
2024-06-12 12:51:46 +0800

    fix umi template

--- template/umi/index.tsx
+++ template/umi/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import less from "./index.less";
 export default function Index() {
-  return <div className={less.container}>name</div>;
+  return <div className="container">name</div>;
 }
