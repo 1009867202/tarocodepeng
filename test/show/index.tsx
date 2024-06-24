6d79ba18cb2caeb9f9cd9c5ceed5ae1750184625
leviRa3 <levi@wizmacau.com>
2024-06-24 12:05:39 +0800

    get all

---
+++ test/show/index.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
+
+const index = () => {
+  return <div></div>;
+};
+
+export default index;
---
+++ test/show/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
+import less from "./index.module.scss";
+export default function Index() {
+  return <div className={less.container}>name</div>;
+}
