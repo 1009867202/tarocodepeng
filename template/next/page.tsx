987fac7f7d7aea47e561fac5643166affc7fede4
leviRa3 <levi@wizmacau.com>
2024-08-29 12:34:53 +0800

    fix next template

--- template/next/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import React, { useState, useEffect, useCallback, useMemo } from "react";
-import less from "./index.module.scss";
-export default function Index() {
-  return <div className={less.container}>name</div>;
-}
